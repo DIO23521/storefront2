52e0c2002a589b8e6569f5101ea809f6c3a28393
DIO23521 <romashko.dima2003@gmail.com>
2026-03-08 22:49:15 +0100

    Customizing Generic Views

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.ProductDetail.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
     path('collection/', views.CollectionList.as_view(), name='collection-list'),
-    path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
+    path('collection/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
 ]
