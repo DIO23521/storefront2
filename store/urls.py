eba7c0a6658990e51f427e87d85013fab2843ada
DIO23521 <romashko.dima2003@gmail.com>
2026-03-08 21:20:27 +0100

    Generic Views

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:id>/', views.ProductDetail.as_view()),
-    path('collection/', views.collection_list, name='collection-list'),
+    path('collection/', views.CollectionList.as_view(), name='collection-list'),
     path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
 ]
