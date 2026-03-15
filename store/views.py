20f44f6cb21e78fdd5b3b375d2779355aaa6a985
DIO23521 <romashko.dima2003@gmail.com>
2026-03-15 17:29:08 +0100

    Adding a Cart Item

--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ from rest_framework import status
 from .pagination import DefaultPagination
 from .models import Product, Collection, OrderItem, Review, Cart, CartItem
 from .filters import ProductFilter
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, AddCartItemSerializer
 
 # Create your views here.
 
@@ -60,7 +60,15 @@ class CartViewSet(CreateModelMixin, GenericViewSet, DestroyModelMixin, RetrieveM
 
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
+
 
     def get_queryset(self):
         return CartItem.objects.select_related('product').filter(cart_id=self.kwargs['cart_pk'])
