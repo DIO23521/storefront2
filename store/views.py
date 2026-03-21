d084bfd627690a9a0c21bb9306f6d14c7bcd6562
DIO23521 <romashko.dima2003@gmail.com>
2026-03-21 18:17:41 +0100

    Returning the Created Order

--- store/views.py
+++ store/views.py
@@ -95,13 +95,19 @@ class CustomerViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(
+            data=request.data,
+            context={'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
     def get_serializer_class(self, *args, **kwargs):
         if self.request.method == 'POST':
             return CreateOrderSerializer
         return OrderSerializer
-
-    def get_serializer_context(self):
-        return {'user_id': self.request.user.id}
 
     def get_queryset(self):
 
